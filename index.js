9d1c401669911306e64712db03459c6d4a98a975
Sunnie J <71672149+SunnieJ@users.noreply.github.com>
2021-04-08 21:21:15 -0400

    yeah....

--- index.js
+++ index.js
@@ -78,13 +78,38 @@ const titles = [
 //"--- wrote --- in ---"
 
 //Sort books from oldest to most recent
+const OldtoYoung = books.sort((a, b) => {
+  return a.publishDate - b.publishDate;
+});
+console.log(OldtoYoung);
 
 //sort books alphabetically
+const Alpha = books.sort((a, b) => {
+  let fa = a.name.toLowerCase();
+  let fb = b.name.toLowerCase(); //changes to lowercase, makes it case insensitive
+  return fa < fb ? -1 : fa > fb ? 1 : 0; //puts in alphabetical
+});
+console.log(Alpha);
 
 //Find who wrote War and Peace
+const WnPAuthor = books.find((books) => {
+  if (books.name === "War and Peace")
+    return books.authorFirst + books.authorLast;
+});
+console.log(WnPAuthor);
 
 //how many books were written before 1900?
+const LessThan = books.filter((books) => books.publishDate < 1900);
+const Before1900 = Object.keys(LessThan).length;
+console.log(Before1900);
 
 //was there at least one book published within the last 100 years?
+const within100 = books.filter((books) => books.publishDate > 1921);
+console.log(within100);
 
 //was every book published within the last 100 years?
+if (books[0].publishDate > 1921) {
+  console.log("Yes, every book was published whthin the last 100 years.");
+} else {
+  console.log("No, not every book was published within the last 100 years.");
+}
